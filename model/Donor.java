94c8cd3f278547f2b43b5dd02a7e20a494dc810b
Karen Agustino <117533246+karenagustino@users.noreply.github.com>
2023-06-17 14:50:55 -0700

    model package (cont.)

---
+++ model/Donor.java
@@ -0,0 +1,17 @@
+package project_d7i1y_q3d6f_z8h1l.model;
+
+/**
+ * The intent for this class is to update/store information about a single branch
+ */
+public class Donor {
+    private final int donorID;
+
+    public Donor(int donorID) {
+        this.donorID = donorID;
+    }
+
+    public int getDonorID() {
+        return donorID;
+    }
+
+}
---
+++ model/SelectionCriteria.java
@@ -0,0 +1,29 @@
+package project_d7i1y_q3d6f_z8h1l.model;
+
+/**
+ * The intent for this class is to update/store information about a single branch
+ */
+public class SelectionCriteria {
+    private final int criteriaID;
+    private final String major;
+    private final double GPA;
+    private final int familyIncome;
+
+    public Superintendent(int criteriaID, String major, double GPA, int familyIncome) {
+        this.criteriaID = criteriaID;
+        this.major = major;
+        this.GPA = GPA;
+        this.familyIncome = familyIncome;
+    }
+
+    public int getCriteriaID() {
+        return criteriaID;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public String getGPA() {
+        return GPA;
+    }
+    public String getFamilyIncome() { return familyIncome; }
+}
