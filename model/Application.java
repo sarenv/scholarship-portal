dcebf04affbbf46abdd9369ceb4e278e2db23c8e
Karen Agustino <117533246+karenagustino@users.noreply.github.com>
2023-06-18 13:29:26 -0700

    model package (application.java added FK)

--- model/Application.java
+++ model/Application.java
@@ -5,15 +5,18 @@ package project_d7i1y_q3d6f_z8h1l.model;
  */
 public class Application {
     private final int applicationID;
+    private final int applicantID;
     private final float deadline; // no date
 
-    public Application(int applicationID, float deadline) {
+    public Application(int applicationID, int applicantID, float deadline) {
         this.applicationID = applicationID;
+        this.applicantID = applicantID;
         this.deadline = deadline;
     }
 
     public int getApplicationID() {
         return applicationID;
     }
+    public int getApplicantID() { return applicantID; }
     public int getDeadline() { return deadline; }
 }
