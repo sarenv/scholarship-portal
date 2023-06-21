cc2f356fc04b0da0c70e38a24c5dd039cd7c3742
Shaun-F-net <shaunfoo560@gmail.com>
2023-06-20 20:54:48 -0700

    base template ui

---
+++ ui/ScholarshipGUI.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.*;
+
+public class ScholarshipGUI extends JFrame {
+    // Put buttons and shit and instantiate other shit here
+    public ScholarshipGUI() {
+        // Initializers and this.adds go in here
+    }
+
+    // More methods
+
+}
